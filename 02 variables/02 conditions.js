a8494bb00e8f92316d87ba01fc61cd826105dde9
Sai Suresh <83501967+saisuresh1209@users.noreply.github.com>
2023-11-01 22:59:12 +0530

    errorcorrection

--- 02 variables/02 conditions.js
+++ 02 variables/02 conditions.js
@@ -2,14 +2,14 @@ let isLoggedInFromFacebook = true;
 
 let isLoggedInFromGoogle = true;
 
-// if (isLoggedInFromFacebook){
-//     console.log("welcome to landing page. please process your request");
-// }
-// if (isLoggedInFromGoogle){
-//     console.log("welcome to landing page. please process your request");
-// }else{
-//     console.log("please create your account");
-// }
+if (isLoggedInFromFacebook){
+    console.log("welcome to landing page. please process your request");
+}
+if (isLoggedInFromGoogle){
+    console.log("welcome to landing page. please process your request");
+}else{
+    console.log("please create your account");
+}
 
 if (isLoggedInFromFacebook && isLoggedInFromGoogle){
     console.log("welcome to landing page");
