0cdc6702775d0b6b8f48ae6076eeb7379ea37ed0
Sai Suresh <83501967+saisuresh1209@users.noreply.github.com>
2023-11-02 23:47:47 +0530

    add

--- 02 variables/02 conditions.js
+++ 02 variables/02 conditions.js
@@ -2,17 +2,21 @@ let isLoggedInFromFacebook = true;
 
 let isLoggedInFromGoogle = true;
 
-if (isLoggedInFromFacebook){
-    console.log("welcome to landing page. please process your request");
-}
-if (isLoggedInFromGoogle){
-    console.log("welcome to landing page. please process your request");
-}else{
-    console.log("please create your account");
-}
+// if (isLoggedInFromFacebook){
+//     console.log("welcome to landing page. please process your request");
+// }
+// if (isLoggedInFromGoogle){
+//     console.log("welcome to landing page. please process your request");
+// }else{
+//     console.log("please create your account");
+// }
 
 if (isLoggedInFromFacebook && isLoggedInFromGoogle){
     console.log("welcome to landing page");
 }else{
     console.log("craete a account to access the banking form");
 }
+
+var authenticated = true;
+
+authenticated ? console.log("show logout button") : console.log("show login button");
