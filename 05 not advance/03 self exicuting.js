999e40f0b0d6cca58639e5ff5dbcae87c6c7bb3d
Sai Suresh <83501967+saisuresh1209@users.noreply.github.com>
2023-11-22 22:45:19 +0530

    selfexi

---
+++ 05 not advance/03 self exicuting.js
@@ -0,0 +1,16 @@
+(function (){
+    console.log("hello there!");
+    console.log("hello there! i'm Saisuresh");
+})();
+
+function init(){
+    var firstName = "Saisuresh";
+    function sayName(){
+        console.log(firstName);
+    }
+    sayName();
+}
+
+init();
+
+console.log(firstName);
